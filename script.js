5287882fe2ff2ad303d450717d4ab971d0038665
DCoder117 <dhirajopjadhav17@gmail.com>
2026-09-09 22:03:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const age = document.getElementById("age").value;
+    const name = document.getElementById("name").value;
+
+    // Validate inputs
+    if (age === "" || name.trim() === "") {
+        alert("Please enter valid details.");
+        return;
+    }
+
+    // Create Promise
+    const promise = new Promise(function (resolve, reject) {
+
+        setTimeout(function () {
+
+            if (Number(age) > 18) {
+                resolve(name);
+            } else {
+                reject(name);
+            }
+
+        }, 4000);
+
+    });
+
+    // Promise handling
+    promise
+        .then(function (name) {
+            alert("Welcome " + name + ". You can vote.");
+        })
+        .catch(function (name) {
+            alert("Oh sorry " + name + ". You aren't old enough.");
+        });
+});
